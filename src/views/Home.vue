6ac7e7b6ebe4fdd9ed080906fd47611d32309936
mathilde <mathilde.pascal411@gmail.com>
2025-02-11 22:26:25 +0100

    Completed homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,51 @@
 <template>
   <h1>Hello ! The name's Mathilde !</h1>
   <p>
-    I am a software engineer, ans I try doing stuff... sometimes.
+    I am a software engineer, and I try doing stuff... sometimes. I'm pretty much interested in
+    anything that has to do with computers. I'll try to use this website to share the things I like
+    and am working on.
   </p>
+  <h2>My projects</h2>
+  <p>Here is my project dump. Most of them are unfinished (or even barely started), some other have
+    been made with other people.</p>
+  <ul>
+    <li>
+      <a href="https://github.com/Mathilde411/BoringLang">BoringLang</a> - A VM based fully
+      object-oriented programming language.
+    </li>
+    <li>
+      <a href="https://github.com/kiss-org/kiss">Kiss</a> - A RISC-V microkernel that I'm doing with
+      my girlfriend.
+    </li>
+    <li>
+      <a href="https://cerbair.etu.imt-nord-europe.fr">CerbAIR</a> - A OAuth2 platform for IMT Nord
+      Europe students to login to associative services using their school provided credentials.
+    </li>
+    <li>
+      <a href="https://hypair.imt-ne.fr">HypAIR</a> - The social platform for the associative life
+      of students in IMT Nord Europe. This is a community project of the IT and Networks association
+      of IMT Nord Europe.
+    </li>
+    <li>
+      <a href="https://www.curseforge.com/minecraft/mc-mods/bperipherals">BPeripherals</a> - A
+      minecraft mod to add more peripherals to ComputerCraft such as a database based on SQLite or a
+      Magnetic Card reader.
+    </li>
+    <li>
+      <a href="https://github.com/Mathilde411/PCSC">PCSC++</a> - A C++ library to use ISO/IEC 7816
+      smartcards (very WIP).
+    </li>
+  </ul>
+  <h2>My friends</h2>
+  <p>You can also check out what my friends are doing :)</p>
+  <ul>
+    <li>
+      <a href="https://smnx.fr">Clem</a> - My wonderful girlfriend. She makes her own OS called <a
+      href="https://github.com/skift-org/skift">Skift</a>.
+    </li>
+    <li>
+      <a href="https://hypair.imt-ne.fr/air">AIR</a> - The IT and Networks association of IMT Nord
+      Europe.
+    </li>
+  </ul>
 </template>
