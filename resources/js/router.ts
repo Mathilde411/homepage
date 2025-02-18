2b781737bd68e5f166e46a3430c5cdbbb3f29f42
mathilde <mathilde.pascal411@gmail.com>
2025-02-18 21:42:54 +0100

    Vue router tests

--- resources/js/router.ts
+++ resources/js/router.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home.vue'
-
+console.log(import.meta.env.BASE_URL)
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         {
             path: '/',
